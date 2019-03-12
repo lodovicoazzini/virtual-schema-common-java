a191ed82fc1439293f02111158e7508fc2752f22
Anastasiia Sergienko <anastasiia.sergienko@exasol.com>
2019-03-12 12:52:26 +0100

    #27: try to run ignored test in sonar

--- src/test/java/com/exasol/adapter/json/SqlDataTypeJsonSerializerTest.java
+++ src/test/java/com/exasol/adapter/json/SqlDataTypeJsonSerializerTest.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-class SqlDataTypeJsonSerializerSecondTest {
+class SqlDataTypeJsonSerializerTest {
     @Mock
     DataType dataType;
 
